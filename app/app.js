b03c4aeee83b1734a306f2fe5bfea879f467a1f6
Potapenko Max <mpotapenko.mail@gmail.com>
2022-11-27 15:51:38 +0300

    feature/edit-popup appearence and info copy

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//EDIT POPUP!
 //вытягиваем из документа элементы для отображения edit-popup
 const profileCorrection = document.querySelector('.profile__edit-button');
 const popupProfile = document.querySelector('.overlay_hidden');
@@ -44,3 +45,54 @@ function handleFormSubmit(evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
+
+// Создадим массив объектов данных статических карточек
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const addButton = document.querySelector('.profile__add-button');
+
+
+function renderNewCard(nameValue, urlValue) {
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card');
+
+    const cardImage = document.createElement('img');
+    cardImage.classList.add('card__mask');
+    cardImage.setAttribute(src, urlValue);
+
+    const cardDescription = document.createElement('div');
+    cardContainer.classList.add('card__description');
+
+    const cardTitle = document.createElement('h2');
+    cardTitle.classList.add('card__title');
+    cardTitle.textContent = nameValue;
+
+    const likeButton = document.createElement('button');
+    likeButton.classList.add('card__like');
+    likeButton.setAttribute(type, button);
+}
