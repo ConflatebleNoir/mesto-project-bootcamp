0879af1c0c040918667bc84044b24a4be84072a3
Potapenko Max <mpotapenko.mail@gmail.com>
2022-11-27 12:14:20 +0300

    fix/styles+html(attributes)

--- app/app.js
+++ app/app.js
@@ -1,17 +1,19 @@
 'use strict';
 
 const profileCorrection = document.querySelector('.profile__edit-button');
-const popupProfile = document.querySelector('.overlay');
+const popupProfile = document.querySelector('.overlay_hidden');
 const popupClose = document.querySelector('.edit-form__close');
 
 if (profileCorrection) {
     profileCorrection.addEventListener('click', function () {
-        popupProfile.classList.remove('overlay__hidden');
+        popupProfile.classList.add('overlay');
+        popupProfile.classList.remove('overlay_hidden');
     });
 };
 
 if (popupClose) {
     popupClose.addEventListener('click', function () {
-        popupProfile.classList.add('overlay__hidden');
+        popupProfile.classList.remove('overlay');
+        popupProfile.classList.add('overlay_hidden');
     });
 };
